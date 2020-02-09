fdaaf01058948e713590dc459aba4c4cfa2c5eae
Rares Portan <rares.portan@3pillarglobal.com>
2020-02-09 09:22:54 +0200

    Update

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ minify sandbox.html > dist/sandbox.html
 
 minify settings.js evaluation.js lessons.js navigation.js menu.js editor.js quiz.js > dist/editor.js
 
+cp index.html dist/index.html
 cp lessons.html dist/lessons.html
 sed -i '' '/settings.js/d' dist/lessons.html
 sed -i '' '/evaluation.js/d' dist/lessons.html
